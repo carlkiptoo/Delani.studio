43871b798584f275c328acc4ce7af051d8db9842
carlkiptoo <carlos.kirui@student.moringaschool.com>
2021-07-23 14:19:33 +0300

    modify css html js

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".imageOne").click(function(){
+        $(".paragraphOne").toggle();
+        $(".imageOne").toggle();
+        
+    });
+    $(".imageTwo").click(function(){
+        $(".paragraphTwo").toggle();
+        $(".imageTwo").toggle();
+        
+    });
+    $(".imageThree").click(function(){
+        $(".paragraphThree").toggle();
+        $(".imageThree").toggle();
+        
+    });
+});
